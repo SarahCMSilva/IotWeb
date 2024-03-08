6beec254fe7d2be83008912fbbe6ca9a34b5bceb
SarahCMSilva <sarah.silva5@aluno.senai.br>
2024-03-08 08:17:43 -0300

    IOT

---
+++ constantes.py
@@ -0,0 +1,9 @@
+# variaveis que definem a configuracao do MQTT
+MQTT_HOST = "broker.hivemq.com"
+MQTT_PORT = 1883
+MQTT_USUARIO=""
+MQTT_SENHA=""
+MQTT_TEMPO= 60
+
+# variaveis que definem a configuracao dos topicos
+TOPICO_STATUS_SEMAFORO = "status_semaforo/33"
